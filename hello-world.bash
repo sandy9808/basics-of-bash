507b10bf80333dbd29c5ca169c3b66366a204e98
sandy9808 <sandeepkumar070200@gmail.com>
2020-08-19 11:36:30 +0000

    First file

---
+++ hello-world.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Hello there"
+echo "------------"
+echo "I hope this is your last time"
+echo "pwd-----------------------"
+pwd
+echo "ls-----------------------"
+ls
+echo "date-----------------------"
+date
+echo "ls -l /boot----------------"
+ls -l /boot
+echo "exit 0-----------------------"
+exit 0
